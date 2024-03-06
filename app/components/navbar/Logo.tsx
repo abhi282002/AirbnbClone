7c1d0019dc9eda3c956f873e5efed53375401239
Abhishek Sharma <sharma312006@gmail.com>
2024-03-06 22:58:12 +0530

    build Navbar of Component

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,18 @@
+"use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+const Logo = () => {
+  return (
+    <div>
+      <Image
+        alt="Logo"
+        className="hidden md:block cursor-pointer"
+        height={"100"}
+        width={"100"}
+        src={"/images/Logo.png"}
+      />
+    </div>
+  );
+};
+
+export default Logo;
