bb26e4b3f1b1704f0fd2f299c2962e197f86d067
Abhishek Sharma <sharma312006@gmail.com>
2024-03-07 08:47:08 +0530

    zustand setup

---
+++ app/components/models/RegisterModel.tsx
@@ -0,0 +1,11 @@
+"use client";
+import axios from "axios";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { useCallback, useState } from "react";
+import { FieldValue, SubmitHandler, useForm } from "react-hook-form";
+const RegisterModel = () => {
+  return <div></div>;
+};
+
+export default RegisterModel;
