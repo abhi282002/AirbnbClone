025a8728e2374f66d002afa383f882eade7028d2
Abhishek Sharma <sharma312006@gmail.com>
2024-03-29 15:54:49 +0530

    reservation cancel creation successfull

---
+++ app/reservations/ReservationClient.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Container from "../components/Container";
+import Heading from "../components/Heading";
+
+const ReservationClient = () => {
+  return (
+    <Container>
+      <Heading title="Reservations" subtitle="Bookings on your properties" />
+    </Container>
+  );
+};
+
+export default ReservationClient;
