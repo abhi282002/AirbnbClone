926c16a79b71ff652cae872dde2b0e3618da28a7
Abhishek Sharma <sharma312006@gmail.com>
2024-03-30 16:19:28 +0530

    Solve error issue

--- app/components/Inputs/Input.tsx
+++ app/components/Inputs/Input.tsx
@@ -1,7 +1,8 @@
 "use client";
-import React from "react";
-import { FieldError, FieldValues, UseFormRegister } from "react-hook-form";
+
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
 import { BiDollar } from "react-icons/bi";
+
 interface InputProps {
   id: string;
   label: string;
@@ -10,8 +11,9 @@ interface InputProps {
   formatPrice?: boolean;
   required?: boolean;
   register: UseFormRegister<FieldValues>;
-  errors: FieldError;
+  errors: FieldErrors;
 }
+
 const Input: React.FC<InputProps> = ({
   id,
   label,
@@ -27,7 +29,12 @@ const Input: React.FC<InputProps> = ({
       {formatPrice && (
         <BiDollar
           size={24}
-          className="text-neutral-700 absolute top-5 left-2"
+          className="
+            text-neutral-700
+            absolute
+            top-5
+            left-2
+          "
         />
       )}
       <input
@@ -36,18 +43,41 @@ const Input: React.FC<InputProps> = ({
         {...register(id, { required })}
         placeholder=" "
         type={type}
-        className={`peer w-full p-4 pt-6 font-light bg-white border-2 rounded-md outline-none transition disabled:opacity:70 disabled:cursor-not-allowed 
-        ${formatPrice ? "pl-9" : "pl-4"}
-        ${errors[id] ? "border-rose-500" : "border-neutral-300"}
-        ${errors[id] ? "focus:border-rose-500" : "focus:border-neutral-300"}
+        className={`
+          peer
+          w-full
+          p-4
+          pt-6 
+          font-light 
+          bg-white 
+          border-2
+          rounded-md
+          outline-none
+          transition
+          disabled:opacity-70
+          disabled:cursor-not-allowed
+          ${formatPrice ? "pl-9" : "pl-4"}
+          ${errors[id] ? "border-rose-500" : "border-neutral-300"}
+          ${errors[id] ? "focus:border-rose-500" : "focus:border-black"}
         `}
       />
       <label
-        className={`absolute text-md duration-150 -translate-y-3 top-5 z-10 origin-[0]
-        ${
-          formatPrice ? "left-9" : "left-4"
-        } peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4
-        ${errors[id] ? "text-rose-500" : "text-zinc-400"}`}
+        className={`
+          absolute 
+          text-md
+          duration-150 
+          transform 
+          -translate-y-3 
+          top-5 
+          z-10 
+          origin-[0] 
+          ${formatPrice ? "left-9" : "left-4"}
+          peer-placeholder-shown:scale-100 
+          peer-placeholder-shown:translate-y-0 
+          peer-focus:scale-75
+          peer-focus:-translate-y-4
+          ${errors[id] ? "text-rose-500" : "text-zinc-400"}
+        `}
       >
         {label}
       </label>
